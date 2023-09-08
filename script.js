42bead8ca3da8d140a380c911dc052cc69a1ad87
AndreeaPetcuLum <128615938+AndreeaPetcuLum@users.noreply.github.com>
2023-09-08 21:31:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-
-
-const scrollButton = document.getElementById('sageata');
+// Obțineți referința la elementul săgeată
+const scrollButton = document.querySelector('.icbaseline-arrow-downward-icon');
 
 scrollButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    
-    const sectionToScroll = document.getElementById('home'); // Schimbați cu ID-ul secțiunii dorite
-    
+    event.preventDefault(); 
+
+    // Obțineți referința la secțiunea "home"
+    const sectionToScroll = document.querySelector('.home');
+
+    // Verificați dacă secțiunea există
     if (sectionToScroll) {
+        // Derulați către secțiune folosind scrollIntoView cu efect lin
         sectionToScroll.scrollIntoView({ behavior: 'smooth' });
     }
 });
