bde4955d1f1b8bb9df802a7b3c8c0d20c50088d6
AndreeaPetcuLum <128615938+AndreeaPetcuLum@users.noreply.github.com>
2023-09-08 21:18:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,13 @@ function scrollToClass(className) {
     event.preventDefault();
     const element = document.querySelector('.' + className);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+        element.scrollIntoView({ behavior: 'smooth' });
     }
-  }
-
-const sageata = document.getElementById('sageata');
-
+}
 
 const scrollButton = document.getElementById('sageata');
 
-
 scrollButton.addEventListener('click', (event) => {
-    event.preventDefault(); 
-
-    const sectionToScroll = document.querySelector('.home'); 
-
-    
-    if (sectionToScroll) {
-       
-        sectionToScroll.scrollIntoView({ behavior: 'smooth' }); 
-    }
+    scrollToClass('home'); // Derulați la secțiunea cu clasa 'home'
 });
-
 
