d27d87bc53366773b0d352aca69da005141ce9dc
AndreeaPetcuLum <128615938+AndreeaPetcuLum@users.noreply.github.com>
2023-09-15 22:41:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,5 +13,25 @@ scrollButton.addEventListener('click', (event) => {
         sectionToScroll.scrollIntoView({ behavior: 'smooth' });
     }
 });
+// script.js
+
+// Selectează toate elementele cu clasa "projects", "inspiration" și "contact"
+const projectLinks = document.querySelectorAll('.projects, .inspiration, .contact');
+
+// Adaugă un eveniment de clic pe fiecare element
+projectLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        // Obține ID-ul secțiunii țintă din atributul "data-target"
+        const targetId = link.getAttribute('data-target');
+        
+        // Găsește elementul cu ID-ul țintă
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+            // Realizează scroll la secțiunea țintă folosind metoda `scrollIntoView()`
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
 
 
